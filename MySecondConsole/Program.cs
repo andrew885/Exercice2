10c36ab6e24eaa836719bf1f060c9d982d3ebd58
Andrew <Andrew@HOME-PC>
2021-10-12 00:12:23 +0300

    Add project files.

---
+++ MySecondConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MySecondConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal e = 1.414213562373095048M;
+            decimal pi = 3.141592653M;
+            Console.WriteLine($"e = {e} \npi = {pi}");
+        }
+    }
+}
